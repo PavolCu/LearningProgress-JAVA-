90e2818f32bdda761bf2bfa5d825c372e48b6112
Cuninka Pavol <pavol.cuninka@icloud.com>
2023-10-15 19:33:19 +0200

    All teory and topic done...3 stage for completing

---
+++ Topics/Choosing the right collection/Removing duplicates and sorting/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        Set<String> uniqueStrings = new HashSet<>();
+
+        // Read strings and add them to the set
+        for (int i = 0; i < size; i++) {
+            uniqueStrings.add(scanner.next());
+        }
+
+        // Convert the set to a list and sort it lexicographically
+        List<String> sortedStrings = new ArrayList<>(uniqueStrings);
+        Collections.sort(sortedStrings);
+
+        // Print the sorted strings without duplicates
+        for (String s : sortedStrings) {
+            System.out.println(s);
+        }
+    }
+}
