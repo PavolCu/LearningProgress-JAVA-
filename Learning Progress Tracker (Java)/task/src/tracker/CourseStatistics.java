561f3b1311268db533fb7245fb8284ccda958f0b
Pavol Cuninka <pavol.cuninka@icloud.com>
2023-11-26 20:00:53 +0100

    removed duplicity code in CourseStatistics.java

--- Learning Progress Tracker (Java)/task/src/tracker/CourseStatistics.java
+++ Learning Progress Tracker (Java)/task/src/tracker/CourseStatistics.java
@@ -11,7 +11,7 @@ public class CourseStatistics {
         this.studentProgress = studentProgress;
     }
 
-    public String calculateMostPopularCourse() {
+    private Map<String, Integer> calculateCoursePopularity() {
         Map<String, Integer> courseCount = new HashMap<>();
         for (int[] points : studentProgress.getStudentPoints().values()) {
             for (int i = 0; i < points.length; i++) {
@@ -21,14 +21,28 @@ public class CourseStatistics {
                 }
             }
         }
-        if (courseCount.isEmpty()){
-            return "n/a";
+        return courseCount;
+    }
+    private Map<String, Integer> calculateCoursePoints() {
+        Map<String, Integer> coursePoints = new HashMap<>();
+        for (int[] points : studentProgress.getStudentPoints().values()) {
+            for (int i = 0; i < points.length; i++) {
+                String courseName = getCourseIndex(i);
+                coursePoints.put(courseName, coursePoints.getOrDefault(courseName, 0) + points[i]);
+            }
         }
-        int maxCount = Collections.max(courseCount.values());
-        return courseCount.entrySet().stream()
-                .filter(entry -> entry.getValue() == maxCount)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.joining(", "));
+        return coursePoints;
+    }
+    private Map<String, Double> calculateAveragePoints() {
+        Map<String, Double> courseAveragePoints = new HashMap<>();
+        for (int[] points : studentProgress.getStudentPoints().values()) {
+            for (int i = 0; i < points.length; i++) {
+                String courseName = getCourseIndex(i);
+                courseAveragePoints.put(courseName, courseAveragePoints.getOrDefault(courseName, 0.0) + points[i]);
+            }
+        }
+        courseAveragePoints.replaceAll((courseName, totalPoints) -> totalPoints / studentProgress.getStudents().size());
+        return courseAveragePoints;
     }
 
     public String getCourseIndex(int index) {
@@ -48,63 +62,43 @@ public class CourseStatistics {
 
     public String calculateHardestCourse() {
         // Assuming the hardest course is the one with the lowest average points
-        Map<String, Double> courseAveragePoints = new HashMap<>();
-        for (int[] points : studentProgress.getStudentPoints().values()) {
-            for (int i = 0; i < points.length; i++) {
-                String courseName = getCourseIndex(i);
-                courseAveragePoints.put(courseName, courseAveragePoints.getOrDefault(courseName, 0.0) + points[i]);
-            }
-        }
-        courseAveragePoints.replaceAll((courseName, totalPoints) -> totalPoints / studentProgress.getStudents().size());
+        Map<String, Double> courseAveragePoints = calculateAveragePoints();
         return courseAveragePoints.isEmpty() ? "n/a" : Collections.min(courseAveragePoints.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
     public String calculateEasiestCourse() {
         // Assuming the easiest course is the one with the highest average points
-        Map<String, Double> courseAveragePoints = new HashMap<>();
-        for (int[] points : studentProgress.getStudentPoints().values()) {
-            for (int i = 0; i < points.length; i++) {
-                String courseName = getCourseIndex(i);
-                courseAveragePoints.put(courseName, courseAveragePoints.getOrDefault(courseName, 0.0) + points[i]);
-            }
-        }
-        courseAveragePoints.replaceAll((courseName, totalPoints) -> totalPoints / studentProgress.getStudents().size());
+        Map<String, Double> courseAveragePoints = calculateAveragePoints();
         return courseAveragePoints.isEmpty() ? "n/a" : Collections.max(courseAveragePoints.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
     public String calculateLowestActivityCourse() {
         // Assuming the course with the lowest activity is the one with the least points
-        Map<String, Integer> coursePoints = new HashMap<>();
-        for (int[] points : studentProgress.getStudentPoints().values()) {
-            for (int i = 0; i < points.length; i++) {
-                String courseName = getCourseIndex(i);
-                coursePoints.put(courseName, coursePoints.getOrDefault(courseName, 0) + points[i]);
-            }
-        }
+        Map<String, Integer> coursePoints = calculateCoursePoints();
         return coursePoints.isEmpty() ? "n/a" : Collections.min(coursePoints.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
     public String calculateHighestActivityCourse() {
         // Assuming the course with the highest activity is the one with the most points
-        Map<String, Integer> coursePoints = new HashMap<>();
-        for (int[] points : studentProgress.getStudentPoints().values()) {
-            for (int i = 0; i < points.length; i++) {
-                String courseName = getCourseIndex(i);
-                coursePoints.put(courseName, coursePoints.getOrDefault(courseName, 0) + points[i]);
-            }
-        }
+        Map<String, Integer> coursePoints = calculateCoursePoints();
         return coursePoints.isEmpty() ? "n/a" : Collections.max(coursePoints.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
-    public String calculateLeastPopularCourse() {
-        Map<String, Integer> courseCount = new HashMap<>();
-        for (int[] points : studentProgress.getStudentPoints().values()) {
-            for (int i = 0; i < points.length; i++) {
-                if (points[i] > 0) {
-                    String courseName = getCourseIndex(i);
-                    courseCount.put(courseName, courseCount.getOrDefault(courseName, 0) + 1);
-                }
-            }
+
+    public String calculateMostPopularCourse() {
+        Map<String, Integer> courseCount = calculateCoursePopularity();
+
+        if (courseCount.isEmpty()){
+            return "n/a";
         }
+        int maxCount = Collections.max(courseCount.values());
+        return courseCount.entrySet().stream()
+                .filter(entry -> entry.getValue() == maxCount)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining(", "));
+    }
+    public String calculateLeastPopularCourse() {
+        Map<String, Integer> courseCount = calculateCoursePopularity();
+
         if (courseCount.isEmpty()){
             return "n/a";
         }
