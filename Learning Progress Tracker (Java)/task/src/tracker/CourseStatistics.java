ce9b704f8ebcfb73c7446d7b53a18356f95baa64
Pavol Cuninka <pavol.cuninka@icloud.com>
2023-12-02 19:18:02 +0100

    commented CourseStatistics.java

--- Learning Progress Tracker (Java)/task/src/tracker/CourseStatistics.java
+++ Learning Progress Tracker (Java)/task/src/tracker/CourseStatistics.java
@@ -4,14 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+// The CourseStatistics class provides various statistics related to courses.
 public class CourseStatistics {
+    // The student progress data.
     private final StudentProgress studentProgress;
 
+    // Constructor for the CourseStatistics class.
+    // @param studentProgress The student progress data.
     public CourseStatistics(StudentProgress studentProgress) {
 
         this.studentProgress = studentProgress;
     }
 
+    // Calculates the popularity of each course.
+    // @return A map with course names as keys and their popularity as values.
     private Map<String, Integer> calculateCoursePopularity() {
         Map<String, Integer> courseCount = new HashMap<>();
         for (int[] points : studentProgress.getStudentPoints().values()) {
@@ -24,6 +30,9 @@ public class CourseStatistics {
         }
         return courseCount;
     }
+
+    // Calculates the total points of each course.
+    // @return A map with course names as keys and their total points as values.
     private Map<String, Integer> calculateCoursePoints() {
         Map<String, Integer> coursePoints = new HashMap<>();
         for (int[] points : studentProgress.getStudentPoints().values()) {
@@ -34,6 +43,9 @@ public class CourseStatistics {
         }
         return coursePoints;
     }
+
+    // Calculates the average points of each course.
+    // @return A map with course names as keys and their average points as values.
     private Map<String, Double> calculateAveragePoints() {
         Map<String, Double> courseAveragePoints = new HashMap<>();
         for (int[] points : studentProgress.getStudentPoints().values()) {
@@ -46,6 +58,9 @@ public class CourseStatistics {
         return courseAveragePoints;
     }
 
+    // Gets the course name based on the index.
+    // @param index The index of the course.
+    // @return The name of the course.
     private String getCourseName(int index) {
         switch (index) {
             case 0:
@@ -62,18 +77,24 @@ public class CourseStatistics {
     }
 
 
+    // Calculates the hardest course based on the average points.
+    // @return The name of the hardest course.
     public String calculateHardestCourse() {
         // Assuming the hardest course is the one with the lowest average points
         Map<String, Double> courseAveragePoints = calculateAveragePoints();
         return courseAveragePoints.isEmpty() || Collections.min(courseAveragePoints.values()) == 0 ? "n/a" : Collections.min(courseAveragePoints.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
+    // Calculates the easiest course based on the average points.
+    // @return The name of the easiest course.
     public String calculateEasiestCourse() {
         // Assuming the easiest course is the one with the highest average points
         Map<String, Double> courseAveragePoints = calculateAveragePoints();
         return courseAveragePoints.isEmpty() || Collections.max(courseAveragePoints.values()) == 0 ? "n/a" : Collections.max(courseAveragePoints.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
+    // Calculates the course with the lowest activity based on the total points.
+    // @return The name of the course with the lowest activity.
     public String calculateLowestActivityCourse() {
         Map<String, Integer> coursePoints = calculateCoursePoints();
         if (coursePoints.isEmpty()) {
@@ -90,6 +111,9 @@ public class CourseStatistics {
 
         return hasCourseWithMinPoints ? "n/a" : lowestActivityCourses.get(0);
     }
+
+    // Calculates the course with the highest activity based on the total points.
+    // @return The name of the course with the highest activity.
     public String calculateHighestActivityCourse() {
         Map<String, Integer> coursePoints = calculateCoursePoints();
         if (coursePoints.isEmpty()) {
@@ -100,6 +124,9 @@ public class CourseStatistics {
                 .map(Map.Entry::getKey)
                 .collect(Collectors.joining(", "));
     }
+
+    // Calculates the most popular course based on the popularity.
+    // @return The name of the most popular course.
     public String calculateMostPopularCourse() {
         Map<String, Integer> courseCount = calculateCoursePopularity();
 
@@ -111,6 +138,9 @@ public class CourseStatistics {
                 .map(Map.Entry::getKey)
                 .collect(Collectors.joining(", "));
     }
+
+    // Calculates the least popular course based on the popularity.
+    // @return The name of the least popular course.
     public String calculateLeastPopularCourse() {
         Map<String, Integer> courseCount = calculateCoursePopularity();
 
